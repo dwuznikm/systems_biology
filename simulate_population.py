9c6718d2f450723888eebe354ea6327438f959f3
Aleksandra Prorok <aleksandraa.prorok@gmail.com>
2024-04-11 12:44:53 +0200

    Added PCA

--- simulate_population.py
+++ simulate_population.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import os
 import simulation_tools as st
 
+
 #parser json -  parametru
 # DEADLINE 16 KWIETNIA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
 # próba komentarza
@@ -17,7 +18,8 @@ import simulation_tools as st
 # ZROBIE PCA OBIECUJE
 
 
-def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=np.random.random()):
+def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.uniform(0.35, 1)):
+  print(mi)
   # Create a folder to store the results
   current_time = datetime.now().strftime("%d.%m.%Y-%H-%M-%S")
   results_folder = 'results'
@@ -34,45 +36,64 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=np.random.ra
       genotype_df.loc[i] = genotype
       phenotype_df.loc[i] = st.fitness(optimal_genotype, genotype)
 
-  # life loop
+    # life loop
 
   population_size = {'x':[], 'y':[]}
   phenotypes = [phenotype_df.mean()]
   population_sizes = [phenotype_df.shape[0]]
+  genotype_copy =  genotype_df.copy()
+  #genotype_copy.loc[len(genotype_copy)] = optimal_genotype
+  pca_df_list=[(st.pca(genotype_copy))]
+  opt_df = pd.DataFrame(columns=[f'cecha{str(i)}' for i in range(n)])
+  opt_df.loc[0] = optimal_genotype
+
+  new_optimal_genotype = optimal_genotype
   for pokolenie in range(time):
+    birth_list = []
+    new_optimal_genotype = st.calculate_optimal_genotype(mi, new_optimal_genotype, opt_genotype_sd, n)
+    for i in range(phenotype_df.shape[0]):
+        if phenotype_df.iloc[i].phenotype < max_fitness:
+          children_count = st.children_roullete(phenotype_df.iloc[i].phenotype, max_fitness)
+          for child in range(children_count):
+            new_genotype = []
+            for j in range(genotype_df.shape[1]):
+                new_value = st.mutation(mi, genotype_df.iloc[i, j], pop_genotype_sd)
+                new_genotype.append(new_value)
+            birth_list.append((new_genotype, st.fitness(new_optimal_genotype, new_genotype)))
+    genotype_df.drop(genotype_df.index, inplace=True)
+    phenotype_df.drop(phenotype_df.index, inplace=True)
+    for i in range(len(birth_list)):
+      genotype_df.loc[i] = birth_list[i][0]
+      phenotype_df.loc[i] = birth_list[i][1]
+    phenotypes.append(phenotype_df.mean())
+    population_sizes.append(phenotype_df.shape[0])
+    max_fitness = st.calculate_max_fitness(phenotype_df.shape[0])
+    if genotype_df.shape[0] == 0:
+      time = pokolenie
+      break
+    genotype_copy =  genotype_df.copy()
+    #genotype_copy.loc[len(genotype_copy)] = new_optimal_genotype
+    pca_df_list.append(st.pca(genotype_copy))
+    opt_df.loc[len(opt_df)]=new_optimal_genotype
 
-      birth_list = []
-      new_optimal_genotype = st.calculate_optimal_genotype(mi, optimal_genotype, opt_genotype_sd, n)
-      for i in range(phenotype_df.shape[0]):
-          if phenotype_df.iloc[i].phenotype < max_fitness:
-            children_count = st.children_roullete(phenotype_df.iloc[i].phenotype, max_fitness)
-            for child in range(children_count):
-              new_genotype = []
-              for j in range(genotype_df.shape[1]):
-                  new_value = st.mutation(mi, genotype_df.iloc[i, j], pop_genotype_sd)
-                  new_genotype.append(new_value)
-              birth_list.append((new_genotype, st.fitness(new_optimal_genotype, new_genotype)))
-      genotype_df.drop(genotype_df.index, inplace=True)
-      phenotype_df.drop(phenotype_df.index, inplace=True)
-      for i in range(len(birth_list)):
-        genotype_df.loc[i] = birth_list[i][0]
-        phenotype_df.loc[i] = birth_list[i][1]
-      phenotypes.append(phenotype_df.mean())
-      population_sizes.append(phenotype_df.shape[0])
-      max_fitness = st.calculate_max_fitness(phenotype_df.shape[0])
 
-
-      print(f'Pokolenie {pokolenie+1} AVG Fitness: {round(phenotype_df.mean().values[0], 2)}, max_fitness: {max_fitness} Population: {phenotype_df.shape[0]}')
+    print(f'Pokolenie {pokolenie+1} AVG Fitness: {round(phenotype_df.mean().values[0], 2)}, max_fitness: {max_fitness} Population: {phenotype_df.shape[0]}')
 
   phenotypes_values = [series.values[0] for series in phenotypes]
 
-  st.create_plot(phenotypes_values, time, subfolder_path, data_type='fitness')
-  st.create_plot(population_sizes, time, subfolder_path, data_type='population')
+  # st.create_plot(phenotypes_values, time, subfolder_path, data_type='fitness')
+  # st.create_plot(population_sizes, time, subfolder_path, data_type='population')
+  
+  opt_pca = st.pca(opt_df)
+  print(opt_pca)
+  st.pca_scatter(pca_df_list, opt_pca, time)
+  
+  
 
 N0=500
 n=5
-time=10
-opt_genotype_sd=0.5
-pop_genotype_sd=0.5
+time=100
+opt_genotype_sd=0.1
+pop_genotype_sd=0.8
 
 simulate(N0, n, time, opt_genotype_sd, pop_genotype_sd)
--- simulation_tools.py
+++ simulation_tools.py
@@ -7,6 +7,12 @@ from matplotlib.animation import FuncAnimation
 from datetime import datetime
 import os
 
+#pca
+from sklearn import datasets, decomposition
+from sklearn.decomposition import PCA
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 def random_genotype(n):
   return [random.uniform(-10, 10) for i in range(n)]
@@ -28,23 +34,22 @@ def mutation(mi, genotype, strenght):
 
 
 def calculate_optimal_genotype(mi, genotype, strenght, n):
-  if np.random.random() < mi:
-    p = np.random.normal(loc=0.3, scale=strenght, size=n)
-    genotype += p
+  p = np.random.normal(loc=0.2, scale=strenght, size=n)
+  genotype += p
   return genotype
 
 def children_roullete(fitness, max_fitness):
   exponent = (max_fitness - fitness)/15
   l = np.exp(exponent)
   children_amount = np.random.poisson(lam=l)
-  if children_amount > 3:
-    return 3
-  else:
-    return children_amount
+  #if children_amount > 3:
+  #  return 3
+  #else:
+  return children_amount
 
 # zmienić z liniowej
 def calculate_max_fitness(population):
-  return max(15 - population * 0.015, 2)
+  return max(25 - population * 0.04, 2)
 
 
 def create_plot(values, time, subfolder_path, data_type):
@@ -74,3 +79,53 @@ def create_plot(values, time, subfolder_path, data_type):
   animation = FuncAnimation(fig, update, frames=frames, interval=500)
   animation.save(os.path.join(subfolder_path, f'{data_type}.gif'), writer='pillow')
   fig.savefig(os.path.join(subfolder_path, f'{data_type}_final.jpg'))
+
+
+def pca(df, n_of_components=2):
+  scaler = StandardScaler()
+  scaled_data = scaler.fit_transform(df)
+  # Wykonanie PCA
+  pca = PCA(n_of_components)
+  pca_result = pca.fit_transform(scaled_data)
+  # Konwersja wyników PCA do DataFrame
+  pca_df = pd.DataFrame(data=pca_result, columns=['PCA1', 'PCA2'])
+  return pca_df
+
+def pca_scatter(df_list, opt_df, time):
+  fig, ax = plt.subplots(figsize=(10, 7))
+  max_x = 0
+  max_y = 0
+  min_x = 0
+  min_y= 0
+  for pca_df in df_list:
+    maximum_x = pca_df["PCA1"].max()
+    maximum_y = pca_df["PCA2"].max()
+    if maximum_x > max_x:
+      max_x = maximum_x
+    if maximum_y > max_y:
+      max_y = maximum_y
+
+  for pca_df in df_list:
+    minimum_x = pca_df["PCA1"].min()
+    minimum_y = pca_df["PCA2"].min()
+    if minimum_x < min_x:
+      min_x = minimum_x
+    if minimum_y < min_y:
+      min_y = minimum_y
+
+  def update(frame):
+      ax.clear()
+      # ax.scatter(df_list[frame]['PCA1'].iloc[range(df_list[frame].shape[0] - 1)], df_list[frame]['PCA2'].iloc[range(df_list[frame].shape[0] - 1)], color='red', s=5)
+      # ax.scatter(df_list[frame]['PCA1'].iloc[df_list[frame].shape[0]-1], df_list[frame]['PCA2'].iloc[df_list[frame].shape[0]-1], color='green', s=100, marker='s')
+      ax.scatter(df_list[frame]['PCA1'], df_list[frame]['PCA2'], color='red', s=5)
+      ax.scatter(opt_df['PCA1'].iloc[frame], opt_df['PCA2'].iloc[frame], color='green', marker='s', s=100)
+      ax.set_title(f'Genotypy osobników w pokoleniu {str(frame)}')
+      ax.tick_params(axis='x')
+      ax.set_ylim(1.15*min_y, 1.15 * max_y)
+      ax.set_xlim(1.15*min_x, 1.15 * max_x)
+      
+    
+  frames = range(time+1)
+  animation = FuncAnimation(fig, update, frames=frames, interval=500)
+  animation.save("proba.gif", writer='pillow')
+  
