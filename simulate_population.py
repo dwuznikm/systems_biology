bfbca235364b9f9c583f50b1ae965972ce215d31
Marcin Dwużnik <dwuznik.marcin@gmail.com>
2024-04-14 16:19:57 +0200

    Added more modes

--- simulate_population.py
+++ simulate_population.py
@@ -9,24 +9,14 @@ import os
 import simulation_tools as st
 
 
-#parser json -  parametru
-# DEADLINE 16 KWIETNIA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
-# próba komentarza
 
-# Funkcja aktualizacji max_fitness na podstawie wielkości populacji
-# porobić symulacje z różnym sd dla opt_genotypu i pop_genotypu
-# ZROBIE PCA OBIECUJE
-
-
-def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.uniform(0.35, 1)):
-  print(mi)
-  # Create a folder to store the results
+def simulate(N, n, time, opt_genotype_sd, pop_genotype_sd, speed, resources, mi=random.uniform(0.35, 1), meteor_chance=0):
   current_time = datetime.now().strftime("%d.%m.%Y-%H-%M-%S")
   results_folder = 'results'
   subfolder_path = os.path.join(results_folder, current_time)
   os.makedirs(subfolder_path)
 
-  max_fitness = st.calculate_max_fitness(N)
+  max_fitness = st.calculate_max_fitness(N, n, resources)
   optimal_genotype = st.random_genotype(n)
   genotype_df = pd.DataFrame(columns=[f'cecha{str(i)}' for i in range(n)])
   phenotype_df = pd.DataFrame(columns=['phenotype'])
@@ -35,8 +25,6 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.unifo
       genotype = st.random_genotype(n)
       genotype_df.loc[i] = genotype
       phenotype_df.loc[i] = st.fitness(optimal_genotype, genotype)
-
-    # life loop
 
   population_size = {'x':[], 'y':[]}
   phenotypes = [phenotype_df.mean()]
@@ -48,7 +36,7 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.unifo
   new_optimal_genotype = optimal_genotype
   for pokolenie in range(time):
     birth_list = []
-    new_optimal_genotype = st.calculate_optimal_genotype(mi, new_optimal_genotype, opt_genotype_sd, n)
+    new_optimal_genotype = st.meteor(mi, new_optimal_genotype, opt_genotype_sd, n, speed, meteor_chance)
     for i in range(phenotype_df.shape[0]):
         if phenotype_df.iloc[i].phenotype < max_fitness:
           children_count = st.children_roullete(phenotype_df.iloc[i].phenotype, max_fitness)
@@ -65,10 +53,10 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.unifo
       phenotype_df.loc[i] = birth_list[i][1]
     phenotypes.append(phenotype_df.mean())
     population_sizes.append(phenotype_df.shape[0])
-    max_fitness = st.calculate_max_fitness(phenotype_df.shape[0])
     if genotype_df.shape[0] == 0:
       time = pokolenie
       break
+    max_fitness = st.calculate_max_fitness(phenotype_df.shape[0], n, resources)
     pca_df_list.append(genotype_df.copy())
     opt_df.loc[len(opt_df)]=new_optimal_genotype
 
@@ -80,9 +68,9 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.unifo
   st.create_plot(population_sizes, time, subfolder_path, data_type='population')
   if n!=2:
     pca_pop_and_opt_gen = st.perform_pca(pca_df_list, opt_df)
-    st.pca_scatter(pca_pop_and_opt_gen, time, subfolder_path)
+    st.pca_scatter(pca_pop_and_opt_gen, population_sizes, time, subfolder_path)
   elif n==2:
-    st.two_dim_scatter(pca_df_list, opt_df, time, subfolder_path)
+    st.two_dim_scatter(pca_df_list, opt_df, population_sizes, time, subfolder_path)
   
   
   
@@ -91,7 +79,9 @@ def simulate(N, n, time, opt_genotype_sd=1, pop_genotype_sd=0.5, mi=random.unifo
 N0=500
 n=2
 time=100
-opt_genotype_sd=0.5
-pop_genotype_sd=0.8
+opt_genotype_sd=0.2
+speed=0.2
+pop_genotype_sd=0.5
+resources = 'Many resources'
 
-simulate(N0, n, time, opt_genotype_sd, pop_genotype_sd)
+simulate(N0, n, time, opt_genotype_sd, pop_genotype_sd, speed, resources, 1, 0.5)
--- simulation_tools.py
+++ simulation_tools.py
@@ -20,29 +20,43 @@ def fitness(optimal_genotype, genotype):
         raise ValueError("Długość wektorów musi być taka sama.")
     suma_kwadratow_roznicy = sum((a - b)**2 for a, b in zip(optimal_genotype, genotype))
     fitness_value = math.sqrt(suma_kwadratow_roznicy)
+    fitness_value /= len(optimal_genotype)
     return fitness_value
 
 
 def mutation(mi, genotype, strenght):
-  p = np.random.normal(loc=0, scale=strenght)
-  genotype += p
+  if random.random() < mi:
+    p = np.random.normal(loc=0, scale=strenght)
+    genotype += p
   return genotype
 
 
-def calculate_optimal_genotype(mi, genotype, strenght, n):
-  p = np.random.normal(loc=0.3, scale=strenght, size=n)
-  genotype += p
+def calculate_optimal_genotype(mi, genotype, strenght, n, speed):
+  if random.random() < mi:
+    p = np.random.normal(loc=speed, scale=strenght, size=n)
+    genotype += p
   return genotype
+
+def meteor(mi, genotype, strenght, n, speed, meteor_chance):
+  if random.random() > meteor_chance:
+    return calculate_optimal_genotype(mi, genotype, strenght, n, speed)
+  else:
+    print('meteor')
+    return calculate_optimal_genotype(1, genotype, strenght*3, n, speed*12)
 
 def children_roullete(fitness, max_fitness):
   exponent = (max_fitness - fitness)/15
   l = np.exp(exponent)
   children_amount = np.random.poisson(lam=l)
-  return children_amount
+  return min(8, children_amount)
 
-# zmienić z liniowej
-def calculate_max_fitness(population):
-  return max(15 - population * 0.02, 2)
+def calculate_max_fitness(population, n, mode):
+  if mode == 'Limited resources':
+    return min(max(500/population, 1), 13)/n
+  elif mode == 'Standard':
+    return min(max(1200/population, 2), 14)/n
+  elif mode == 'Many resources':
+    return min(max(2000/population, 3), 15)/n
 
 
 def create_plot(values, time, subfolder_path, data_type):
@@ -86,7 +100,7 @@ def perform_pca(population_list, optimal_genotype_df):
     pca_list.append(pca_df)
   return pca_list
 
-def two_dim_scatter(df_list, opt_df, time, subfolder_path):
+def two_dim_scatter(df_list, opt_df, population_sizes, time, subfolder_path):
   fig, ax = plt.subplots(figsize=(10, 7))
   max_x = 0
   max_y = 0
@@ -99,6 +113,10 @@ def two_dim_scatter(df_list, opt_df, time, subfolder_path):
       max_x = maximum_x
     if maximum_y > max_y:
       max_y = maximum_y
+  if opt_df["cecha0"].max() > max_x:
+    max_x = opt_df["cecha0"].max()
+  if opt_df["cecha1"].max() > max_y:
+    max_y = opt_df["cecha1"].max()
 
   for df in df_list:
     minimum_x = df["cecha0"].min()
@@ -108,11 +126,17 @@ def two_dim_scatter(df_list, opt_df, time, subfolder_path):
     if minimum_y < min_y:
       min_y = minimum_y
 
+  if opt_df["cecha0"].min() < min_x:
+    min_x = opt_df["cecha0"].min()
+  if opt_df["cecha1"].min() < min_y:
+    min_y = opt_df["cecha1"].min()
+
   def update(frame):
       ax.clear()
       ax.scatter(df_list[frame]['cecha0'], df_list[frame]['cecha1'], color='blue', label='Population', alpha=0.5)
       ax.scatter(opt_df['cecha0'].iloc[frame], opt_df['cecha1'].iloc[frame], color='red', marker='s', s=100, label='Optimal Genotype')
       ax.set_title(f'Genotypy osobników w pokoleniu {str(frame)}')
+      ax.text(1, 1, f'Population size: {population_sizes[frame]}')
       ax.tick_params(axis='x')
       ax.set_ylim(1.15*min_y, 1.15 * max_y)
       ax.set_xlim(1.15*min_x, 1.15 * max_x)
@@ -121,7 +145,7 @@ def two_dim_scatter(df_list, opt_df, time, subfolder_path):
   animation = FuncAnimation(fig, update, frames=frames, interval=500)
   animation.save(os.path.join(subfolder_path, f'2d.gif'), writer='pillow')
 
-def pca_scatter(df_list, time, subfolder_path):
+def pca_scatter(df_list, time, population_sizes, subfolder_path):
   fig, ax = plt.subplots(figsize=(10, 7))
   max_x = 0
   max_y = 0
@@ -148,6 +172,8 @@ def pca_scatter(df_list, time, subfolder_path):
       ax.scatter(df_list[frame]['PCA1'][:-1], df_list[frame]['PCA2'][:-1], color='blue', label='Population', alpha=0.5)
       ax.scatter(df_list[frame].iloc[-1]['PCA1'], df_list[frame].iloc[-1]['PCA2'], color='red', marker='s', s=100, label='Optimal Genotype')
       ax.tick_params(axis='x')
+      #fix placement
+      ax.text(0, 0.95, f'Population size: {population_sizes[frame]}')
       ax.set_ylim(1.15*min_y, 1.15 * max_y)
       ax.set_xlim(1.15*min_x, 1.15 * max_x)
       
